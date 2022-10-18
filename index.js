a8d2f160e36d92ee8ed8d9a8c08b3f8a232d98b2
Ben <baziz10sa@gmail.com>
2022-10-18 12:58:24 -0700

    mouse

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const containerEl = document.querySelector
-(".container")
+const containerEl = document.querySelector(".container")
 
 window.addEventListener("mousemove", 
 (event) => {
     containerEl.innerHTML = `
-         <div class="mouse-event">
+        <div class="mouse-event">
          ${event.clientX}
          <h4>Mouse X Position(px)</h4>
         </div>
